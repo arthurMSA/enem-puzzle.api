cb228277ff1dae57da47dc9ec5b3de8f6da3bd30
arthurMSA <arthurmarcelosantanaalves@hotmail.com>
2024-02-23 02:00:15 -0300

    chore: remove app CORS origin

--- src/main.py
+++ src/main.py
@@ -17,16 +17,12 @@ async def lifespan(app: FastAPI):
 
 app = FastAPI(lifespan=lifespan)
 
-origins = [
-    os.environ.get('APP_URL')
-]
-
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
     allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
+    allow_origins=['*'],
+    allow_methods=['*'],
+    allow_headers=['*'],
 )
 
 app.include_router(questions.router)
