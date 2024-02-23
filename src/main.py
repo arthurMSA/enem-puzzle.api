3584265e830a81bf2ddf4c77bbefbe5f873ec748
arthurMSA <arthurmarcelosantanaalves@hotmail.com>
2024-02-23 04:44:22 -0300

    refactor: change main file API

--- src/main.py
+++ src/main.py
@@ -1,28 +1,5 @@
 import uvicorn
-from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
-from API.routes import questions, answer
-from contextlib import asynccontextmanager
-from database.enemDB.settings import enemDBConnection, enemDBDisconnect
-
-@asynccontextmanager
-async def lifespan(app: FastAPI):
-    enemDBConnection()
-    yield
-    enemDBDisconnect()
-
-app = FastAPI(lifespan=lifespan)
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_credentials=True,
-    allow_origins=['*'],
-    allow_methods=['*'],
-    allow_headers=['*'],
-)
-
-app.include_router(questions.router)
-app.include_router(answer.router)
+from API.app import app
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8080)
